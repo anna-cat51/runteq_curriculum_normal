0657f17a52914b453da42988e474e13057069b84
AnnaDoi <willow6510@yahoo.co.jp>
2023-03-17 11:13:31 +0900

    chapter1

--- config/application.rb
+++ config/application.rb
@@ -31,3 +31,15 @@ module RunteqNormal
     config.generators.system_tests = nil
   end
 end
+module XXX
+  class Application < Rails::Application
+    config.load_defaults 5.2
+
+    config.generators do |g|  # ここから追記
+      g.assets false          # CSS, JavaScriptファイル生成せず
+      g.skip_routes true   # trueならroutes.rb変更せず、falseなら通常通り変更
+      g.test_framework false  # testファイル生成せず
+      g.helper false
+    end                       # ここまで
+  end
+end
