a85b9cabd454d7ed67cc0ab8a7362cc44f124903
AnnaDoi <willow6510@yahoo.co.jp>
2023-04-15 21:08:33 +0900

    add_controller_Refactor

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,13 +2,12 @@ class BoardsController < ApplicationController
     before_action :find_board, only: [:edit, :update, :destroy]
 
     def bookmarks
-        @bookmark_boards = current_user.bookmark_boards.includes(:user).order(created_at: :desc)
-        @bookmark_boards = Kaminari.paginate_array(@bookmark_boards).page(params[:page])
+        @bookmark_boards = current_user.bookmark_boards.includes(:user).order(created_at: :desc).page(params[:page])
     end
 
     def index
-        @boards = Board.all.includes(:user).order(created_at: :desc)
-        @boards = Kaminari.paginate_array(@boards).page(params[:page])
+        @boards = Board.all.includes(:user).order(created_at: :desc).page(params[:page])
+
     end
 
     def new
