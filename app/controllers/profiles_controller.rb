ae1313f01dae200ef2db6e83473e06bcc01f15b9
AnnaDoi <willow6510@yahoo.co.jp>
2023-04-21 23:03:30 +0900

    chapter20

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,15 @@
 class ProfilesController < ApplicationController
-    def show
+    before_action :set_user, only: %i[edit update]
 
-    end
+    def show;end
 
-    def edit; end
+    def edit;end
 
     def update
         if @user.update(user_params)
-            redirect_to profile_path, success: "ユーザーを更新しました"
+            redirect_to profile_path, success: t('defaults.message.updated', item: User.model_name.human)
         else
+            flash.now['danger'] = t('defaults.message.not_updated', item: User.model_name.human)
             render :edit
         end
     end
@@ -17,7 +18,7 @@ class ProfilesController < ApplicationController
     def set_user
         @user = User.find(current_user.id)
     end
-    
+
     def user_params
         params.require(:user).permit(:email,:first_name,:last_name,:avatar,:avatar_cache)
     end
