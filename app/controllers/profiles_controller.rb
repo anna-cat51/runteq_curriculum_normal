51399ae93d3419dbb54568d222b7ea934fcc60db
AnnaDoi <willow6510@yahoo.co.jp>
2023-04-21 22:00:04 +0900

    add_controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,24 @@
+class ProfilesController < ApplicationController
+    def show
+
+    end
+
+    def edit; end
+
+    def update
+        if @user.update(user_params)
+            redirect_to profile_path, success: "ユーザーを更新しました"
+        else
+            render :edit
+        end
+    end
+
+    private
+    def set_user
+        @user = User.find(current_user.id)
+    end
+    
+    def user_params
+        params.require(:user).permit(:email,:first_name,:last_name)
+    end
+end
