ef58d36e3c72632ff9ff3e715c4d80fd5a9af451
AnnaDoi <willow6510@yahoo.co.jp>
2023-05-05 18:18:30 +0900

    chapter 22

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  enum role: { general: 0, admin: 1 }
+
   has_many :boards, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
