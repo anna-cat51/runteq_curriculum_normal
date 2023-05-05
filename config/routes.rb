be86e02585a6373ca31367c6e7e09931e42b7253
AnnaDoi <willow6510@yahoo.co.jp>
2023-05-05 16:22:54 +0900

    chapter21

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   root 'static_pages#top'
 
   get 'login', to: 'user_sessions#new'
@@ -14,4 +17,6 @@ Rails.application.routes.draw do
   end
 	resources :bookmarks, only: %i[create destroy]
   resource :profile,only: %i[show edit update]
+
+  resources :password_resets, only: %i[new create edit update]
 end
